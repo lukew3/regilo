1412f904a61503209cc95f9015e8bea5e60abc15
lukew3 <lukew25073@gmail.com>
2023-08-14 13:08:36 -0400

    wip

--- regilo.cpp
+++ regilo.cpp
@@ -7,11 +7,26 @@
 char* serve_dir = ".";
 
 static void server_callback(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
-        if (ev == MG_EV_HTTP_MSG) {
-		struct mg_http_serve_opts opts = {.root_dir = serve_dir};   // Serve local dir
+	if (ev == MG_EV_HTTP_MSG) {
 		struct mg_http_message *hm = (struct mg_http_message *) ev_data;
-		mg_http_serve_dir(c, hm, &opts);
-	}	
+		if (mg_http_match_uri(hm, "/websocket")) {
+			mg_ws_upgrade(c, hm, NULL);
+		} else {
+			struct mg_http_serve_opts opts = {.root_dir = serve_dir};   // Serve local dir
+			mg_http_serve_dir(c, hm, &opts);
+		}
+	} else if (ev == MG_EV_WS_MSG) {
+		struct mg_ws_message *wm = (struct mg_ws_message *) ev_data;    
+		// Got websocket frame. Received data is wm->data. Echo it back!
+		char* exec_string = "xdotool key ";
+		std::cout << wm->data.ptr << std::endl;
+		strcat(exec_string, wm->data.ptr);
+		std::cout << exec_string << std::endl;
+//		system("xdotool key ctrl+w");
+		system(exec_string);
+		mg_ws_send(c, wm->data.ptr, wm->data.len, WEBSOCKET_OP_TEXT);
+	}
+	(void) fn_data;
 }
 
 void *start_static_server(char* serve_dir) {
